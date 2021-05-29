951ecd4a393f84a86402e5f6e564fba01b2bc35b
Altan Guc <altan.guc@colorado.edu>
2021-05-28 18:09:40 -0600

    adding models data for productTag relation

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -1,19 +1,47 @@
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require("sequelize");
 
-const sequelize = require('../config/connection');
+const sequelize = require("../config/connection");
+const Product = require("./Product");
+const Tag = require("./Tag");
 
 class ProductTag extends Model {}
 
 ProductTag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        // This is a reference to another model
+        model: "product",
+
+        // This is the column name of the referenced model
+        key: "id",
+      },
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        // This is a reference to another model
+        model: "tag",
+
+        // This is the column name of the referenced model
+        key: "id",
+      },
+    },
   },
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'product_tag',
+    modelName: "product_tag",
   }
 );
 
